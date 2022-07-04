f49288fbcabb57b3d8452902e38b5be24512dbb3
Rosterik <rifanwot@gmail.com>
2022-07-04 20:30:53 +0300

    u
    u

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         ArrayList<Character> characterArrayList = new ArrayList<Character>();
         ArrayList<Character> characterArrayList2 = new ArrayList<Character>();
+
+        String s = reader.readLine();
+        char[] list = s.toCharArray();
 
     }
 }
