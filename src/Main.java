0f9078794049f3e124fb4e5fcf3f3a54f73684a1
Rosterik <67566860+Razoxx@users.noreply.github.com>
2022-07-04 20:12:43 +0300

    mod

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Character> characterArrayList = new ArrayList<Character>();
+        ArrayList<Character> characterArrayList2 = new ArrayList<Character>();
+
     }
 }
